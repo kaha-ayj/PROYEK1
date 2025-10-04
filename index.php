242bc500419e6ee5cf70e921eea911a5aa50444c
unknown <khusnul.kh783@gmail.com>
2025-10-04 22:42:15 +0700

    menambahkan kodingan landing page

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+include("includes/header.php");
+?>
+<head>
+    <meta charset="UTF">
+    <meta name="Viewport" content="width=device-width, initial-scale=1.0">
+<title>Lapangin.Aja</title>
+<link rel="stylesheet" href="assets/style.css">
+</head>">
+</head>
+<body>
+    <section class="hero">
+    <div class="text">
+        <h2> Badminton jadi lebih seru, Boking Lapangan tinggal klik!</h2>    
+        <h3> Daripada penuh, mending Lapangin.Aja </h3>
+        <p> 
+            Lapangin.Aja adalah platform penyewaan lapangan Badminton online yang memudahkan
+            kamu untuk booking kapan saja, dan dimana saja. Cek jadwal kosong, pilih lapangan favorite, 
+            dan dapatkan konfirmasi instan. Olahraga jadi lebih mudah dan menyenangkan!
+        </p>
+    </div>
+    <a href="lapangan.php"> <button class="cta"> PILIH LAPANGAN </button></a>
+    <div class="image">
+        <img src="assets/image/pemain.png" alt="pemain Badminton">    
+    </div>
+</section>
+
+</footer>
+</body>
+</html>
