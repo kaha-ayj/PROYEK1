1adf55f37b26b89b7a59902e218a7630f2857b28
unknown <khusnul.kh783@gmail.com>
2025-10-05 22:25:36 +0700

    modifikasi tampilan

--- index.php
+++ index.php
@@ -1,30 +1,31 @@
 <?php
 include("includes/header.php");
 ?>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset="UTF">
-    <meta name="Viewport" content="width=device-width, initial-scale=1.0">
-<title>Lapangin.Aja</title>
-<link rel="stylesheet" href="assets/style.css">
-</head>">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lapangin.Aja</title>
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
     <section class="hero">
     <div class="text">
-        <h2> Badminton jadi lebih seru, Boking Lapangan tinggal klik!</h2>    
-        <h3> Daripada penuh, mending Lapangin.Aja </h3>
-        <p> 
-            Lapangin.Aja adalah platform penyewaan lapangan Badminton online yang memudahkan
-            kamu untuk booking kapan saja, dan dimana saja. Cek jadwal kosong, pilih lapangan favorite, 
-            dan dapatkan konfirmasi instan. Olahraga jadi lebih mudah dan menyenangkan!
-        </p>
+    <h2>Badminton jadi lebih seru, Booking Lapangan tinggal klik!</h2>    
+    <h3>Daripada penuh, mending Lapangin.Aja</h3>
+    <p>
+        Lapangin.Aja adalah platform penyewaan lapangan Badminton online yang memudahkan
+        kamu untuk booking kapan saja, dan di mana saja. Cek jadwal kosong, pilih lapangan favorit, 
+        dan dapatkan konfirmasi instan. Olahraga jadi lebih mudah dan menyenangkan!
+    </p>
+    <a href="lapangan.php"><button class="cta">PILIH LAPANGAN</button></a>
     </div>
-    <a href="lapangan.php"> <button class="cta"> PILIH LAPANGAN </button></a>
+
     <div class="image">
-        <img src="assets/image/pemain.png" alt="pemain Badminton">    
+    <img src="assets/image/pemain.png" alt="pemain Badminton">    
     </div>
 </section>
 
-</footer>
 </body>
 </html>
