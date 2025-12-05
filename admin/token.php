e1d6b2f7fed972b71bbadbb0e76f60bdd0e82e59
unknown <khusnul.kh783@gmail.com>
2025-12-05 10:54:04 +0700

    menambahkan midtrans dan modifikasi file

--- admin/token.php
+++ admin/token.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-include 'config/midtrans.php';
+include 'config/koneksi.php'; 
 require_once 'vendor/autoload.php';
 
 \Midtrans\Config::$serverKey = 'SB-MID-SERVER-XXXX';
