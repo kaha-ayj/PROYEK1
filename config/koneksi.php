0f92e997199374d55605ebd1e617780559782422
unknown <khusnul.kh783@gmail.com>
2025-12-24 10:51:22 +0700

    PERBAIKI FILe koneksi

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$host = "sql212.infinityfree.com";
-$user = "if0_40742969";
-$pass = "th41fmsj";
-$db   = "if0_40742969_proyek_1";
+$host = "sql113.infinityfree.com";
+$user = "if0_40751259";
+$pass = "XTlJ8Jq69Vqs9J";
+$db   = "if0_40751259_proyek_1";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
